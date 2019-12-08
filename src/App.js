08fb1b536ce9a6fe5d172849c3a06c8dc3de2f35
Rakshit <1999rakshitsharma@gmail.com>
2019-12-08 16:44:54 +0530

    Dynamic Content: Added 'Delete' Button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import './App.css';
 
 class App extends Component{
   render(){
+
+    
+    let subscribers = [
+      {
+        id: 1,
+        name: "Shilpa Bhat",
+        phone: "8888888888"
+      },
+      {
+        id: 2,
+        name: "Srishti Gupta",
+        phone: "9999999999"
+      }
+    ];
+
   return (
    <div>
        <Header />
@@ -14,10 +29,21 @@ class App extends Component{
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
-        </div>
-   </div>
-  );
-}
-}
 
+          {
+            subscribers.map(sub => {
+              return <div key={sub.id} className="grid-container">
+              <span className="grid-item">{sub.name}</span>
+              <span className="grid-item">{sub.phone}</span>
+              <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+              </span>
+        </div>
+            })
+          }
+          </div>
+         </div>
+   );
+  }
+}
 export default App;
